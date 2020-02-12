b3c37adc38619a41234c3a6dd3d52443d2662535
Arek Mamala <mamala132@gmail.com>
2020-02-12 22:51:33 +0000

    looking at types

---
+++ lookatbits.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int main (int argc, char *argv[]){
+  char c =0;
+
+  long int l = 65;
+
+  long long int m = 65;
+  int i =65;
+  uint8_t e = 65;
+  uint64_t s = 65;
+
+  printf("%x\t%c\t%d\n", c, c, c, sizeof(c));
+  printf("%x\t%c\t%d\n", i, i, i, sizeof(i));
+  printf("%x\t%c\t%d\n", l, l, l, sizeof(l));
+  printf("%x\t%c\t%d\n", m, m, m, sizeof(m));
+
+  return 0;
+}
