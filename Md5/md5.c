70effece945dae35b912608f8dc1321bccb3fb41
Arek Mamala <mamala132@gmail.com>
2020-04-03 22:05:10 +0100

    wrong output issue[A

--- Md5/md5.c
+++ Md5/md5.c
@@ -11,6 +11,26 @@
 // Resources
 // https://tools.ietf.org/html/rfc1321
 
+/* Constants for MD5Transform routine.
+ */
+#define S11 7
+#define S12 12
+#define S13 17
+#define S14 22
+#define S21 5
+#define S22 9
+#define S23 14
+#define S24 20
+#define S31 4
+#define S32 11
+#define S33 16
+#define S34 23
+#define S41 6
+#define S42 10
+#define S43 15
+#define S44 21
+
+
 /* ROTATE_LEFT rotates x left n bits.
  */
 #define ROTATE_LEFT(x, n) (((x) << (n)) | ((x) >> (32 - (n))))
@@ -22,12 +42,20 @@
 #define H(x, y, z) ((x) ^ (y) ^ (z))
 #define I(x, y, z) ((y) ^ ((x) | (~z)))
 
+//got from sha256 algorithm
+union block {
+  uint64_t sixfour[8];
+  uint32_t threetwo[16];
+  uint8_t eight[64];
+};
+typedef unsigned long int UINT4;
+
 /* FF, GG, HH, and II transformations for rounds 1, 2, 3, and 4.
 Rotation is separate from addition to prevent recomputation.
  */
 #define FF(a, b, c, d, x, s, ac)                 \
   {                                              \
-    (a) += F((b), (c), (d)) + (x) + ac; \
+    (a) += F((b), (c), (d)) + (x) + (ac); \
     (a) = ROTATE_LEFT((a), (s));                 \
     (a) += (b);                                  \
   }
@@ -49,25 +77,6 @@ Rotation is separate from addition to prevent recomputation.
     (a) = ROTATE_LEFT((a), (s));                 \
     (a) += (b);                                  \
   }
-
-/* Constants for MD5Transform routine.
- */
-#define S11 7
-#define S12 12
-#define S13 17
-#define S14 22
-#define S21 5
-#define S22 9
-#define S23 14
-#define S24 20
-#define S31 4
-#define S32 11
-#define S33 16
-#define S34 23
-#define S41 6
-#define S42 10
-#define S43 15
-#define S44 21
 
 enum flag {READ, PAD0, FINISH};
 
@@ -96,15 +105,6 @@ const uint32_t k[] = {
     0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1,
     0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391};
 
-#define VAR uint32_t
-
-//got from sha256 algorithm
-union block {
-  uint64_t sixfour[8];
-  uint32_t threetwo[16];
-  uint8_t eight[64];
-};
-
 //from sha 256 argorithm chenged to suit md5 p
 int nextblock(union block *M, FILE *infile, uint64_t *nobits, enum flag *status) {
   //padding 
@@ -132,7 +132,7 @@ int nextblock(union block *M, FILE *infile, uint64_t *nobits, enum flag *status)
   if (nobytesread < 56) {
     M->eight[nobytesread] = 0x80;
     for (int i = nobytesread + 1; i < 56; i++)
-      M->eight[i] = 0x00;
+      M->eight[i] = 0;
     M->sixfour[7] = *nobits;
     *status = FINISH;
     return 1;
@@ -148,7 +148,6 @@ int nextblock(union block *M, FILE *infile, uint64_t *nobits, enum flag *status)
 
 static void md5_hash(union block *M, uint32_t *Hash)
 {
-
   uint32_t a, b, c, d;
   a = Hash[0];
   b = Hash[1];
@@ -156,82 +155,80 @@ static void md5_hash(union block *M, uint32_t *Hash)
   d = Hash[3];
 
   /* Round 1 */
-  FF(a, b, c, d, M->threetwo[0], S11, 0xd76aa478);  /* 1 */
-  FF(d, a, b, c, M->threetwo[1], S12, 0xe8c7b756);  /* 2 */
-  FF(c, d, a, b, M->threetwo[2], S13, 0x242070db);  /* 3 */
-  FF(b, c, d, a, M->threetwo[3], S14, 0xc1bdceee);  /* 4 */
-  FF(a, b, c, d, M->threetwo[4], S11, 0xf57c0faf);  /* 5 */
-  FF(d, a, b, c, M->threetwo[5], S12, 0x4787c62a);  /* 6 */
-  FF(c, d, a, b, M->threetwo[6], S13, 0xa8304613);  /* 7 */
-  FF(b, c, d, a, M->threetwo[7], S14, 0xfd469501);  /* 8 */
-  FF(a, b, c, d, M->threetwo[8], S11, 0x698098d8);  /* 9 */
-  FF(d, a, b, c, M->threetwo[9], S12, 0x8b44f7af);  /* 10 */
-  FF(c, d, a, b, M->threetwo[10], S13, 0xffff5bb1); /* 11 */
-  FF(b, c, d, a, M->threetwo[11], S14, 0x895cd7be); /* 12 */
-  FF(a, b, c, d, M->threetwo[12], S11, 0x6b901122); /* 13 */
-  FF(d, a, b, c, M->threetwo[13], S12, 0xfd987193); /* 14 */
-  FF(c, d, a, b, M->threetwo[14], S13, 0xa679438e); /* 15 */
-  FF(b, c, d, a, M->threetwo[15], S14, 0x49b40821); /* 16 */
+  FF(a, b, c, d, M->threetwo[0], S11, k[0]);  /* 1 */
+  FF(d, a, b, c, M->threetwo[1], S12, k[1]);  /* 2 */
+  FF(c, d, a, b, M->threetwo[2], S13, k[2]);  /* 3 */
+  FF(b, c, d, a, M->threetwo[3], S14, k[3]);  /* 4 */
+  FF(a, b, c, d, M->threetwo[4], S11, k[4]);  /* 5 */
+  FF(d, a, b, c, M->threetwo[5], S12, k[5]);  /* 6 */
+  FF(c, d, a, b, M->threetwo[6], S13, k[6]);  /* 7 */
+  FF(b, c, d, a, M->threetwo[7], S14, k[7]);  /* 8 */
+  FF(a, b, c, d, M->threetwo[8], S11, k[8]);  /* 9 */
+  FF(d, a, b, c, M->threetwo[9], S12, k[9]);  /* 10 */
+  FF(c, d, a, b, M->threetwo[10], S13,k[10]); /* 11 */
+  FF(b, c, d, a, M->threetwo[11], S14,k[11]); /* 12 */
+  FF(a, b, c, d, M->threetwo[12], S11,k[12]); /* 13 */
+  FF(d, a, b, c, M->threetwo[13], S12,k[13]); /* 14 */
+  FF(c, d, a, b, M->threetwo[14], S13,k[14]); /* 15 */
+  FF(b, c, d, a, M->threetwo[15], S14,k[15]); /* 16 */
   /* Round 2 */
-  GG(a, b, c, d, M->threetwo[1], S21, 0xf61e2562);  /* 17 */
-  GG(d, a, b, c, M->threetwo[6], S22, 0xc040b340);  /* 18 */
-  GG(c, d, a, b, M->threetwo[11], S23, 0x265e5a51); /* 19 */
-  GG(b, c, d, a, M->threetwo[0], S24, 0xe9b6c7aa);  /* 20 */
-  GG(a, b, c, d, M->threetwo[5], S21, 0xd62f105d);  /* 21 */
-  GG(d, a, b, c, M->threetwo[10], S22, 0x2441453);  /* 22 */
-  GG(c, d, a, b, M->threetwo[15], S23, 0xd8a1e681); /* 23 */
-  GG(b, c, d, a, M->threetwo[4], S24, 0xe7d3fbc8);  /* 24 */
-  GG(a, b, c, d, M->threetwo[9], S21, 0x21e1cde6);  /* 25 */
-  GG(d, a, b, c, M->threetwo[14], S22, 0xc33707d6); /* 26 */
-  GG(c, d, a, b, M->threetwo[3], S23, 0xf4d50d87);  /* 27 */
-  GG(b, c, d, a, M->threetwo[8], S24, 0x455a14ed);  /* 28 */
-  GG(a, b, c, d, M->threetwo[13], S21, 0xa9e3e905); /* 29 */
-  GG(d, a, b, c, M->threetwo[2], S22, 0xfcefa3f8);  /* 30 */
-  GG(c, d, a, b, M->threetwo[7], S23, 0x676f02d9);  /* 31 */
-  GG(b, c, d, a, M->threetwo[12], S24, 0x8d2a4c8a); /* 32 */
+  GG(a, b, c, d, M->threetwo[1], S21, k[16]);  /* 17 */
+  GG(d, a, b, c, M->threetwo[6], S22, k[17]);  /* 18 */
+  GG(c, d, a, b, M->threetwo[11], S23,k[18]); /* 19 */
+  GG(b, c, d, a, M->threetwo[0], S24, k[19]);  /* 20 */
+  GG(a, b, c, d, M->threetwo[5], S21, k[20]);  /* 21 */
+  GG(d, a, b, c, M->threetwo[10], S22,k[21]);  /* 22 */
+  GG(c, d, a, b, M->threetwo[15], S23,k[22]); /* 23 */
+  GG(b, c, d, a, M->threetwo[4], S24, k[23]);  /* 24 */
+  GG(a, b, c, d, M->threetwo[9], S21, k[24]);  /* 25 */
+  GG(d, a, b, c, M->threetwo[14], S22,k[25]); /* 26 */
+  GG(c, d, a, b, M->threetwo[3], S23, k[26]);  /* 27 */
+  GG(b, c, d, a, M->threetwo[8], S24, k[27]);  /* 28 */
+  GG(a, b, c, d, M->threetwo[13], S21,k[28]); /* 29 */
+  GG(d, a, b, c, M->threetwo[2], S22, k[29]);  /* 30 */
+  GG(c, d, a, b, M->threetwo[7], S23, k[30]);  /* 31 */
+  GG(b, c, d, a, M->threetwo[12], S24,k[31]); /* 32 */
 
   /* Round 3 */
-  HH(a, b, c, d, M->threetwo[5], S31, 0xfffa3942);  /* 33 */
-  HH(d, a, b, c, M->threetwo[8], S32, 0x8771f681);  /* 34 */
-  HH(c, d, a, b, M->threetwo[11], S33, 0x6d9d6122); /* 35 */
-  HH(b, c, d, a, M->threetwo[14], S34, 0xfde5380c); /* 36 */
-  HH(a, b, c, d, M->threetwo[1], S31, 0xa4beea44);  /* 37 */
-  HH(d, a, b, c, M->threetwo[4], S32, 0x4bdecfa9);  /* 38 */
-  HH(c, d, a, b, M->threetwo[7], S33, 0xf6bb4b60);  /* 39 */
-  HH(b, c, d, a, M->threetwo[10], S34, 0xbebfbc70); /* 40 */
-  HH(a, b, c, d, M->threetwo[13], S31, 0x289b7ec6); /* 41 */
-  HH(d, a, b, c, M->threetwo[0], S32, 0xeaa127fa);  /* 42 */
-  HH(c, d, a, b, M->threetwo[3], S33, 0xd4ef3085);  /* 43 */
-  HH(b, c, d, a, M->threetwo[6], S34, 0x4881d05);   /* 44 */
-  HH(a, b, c, d, M->threetwo[9], S31, 0xd9d4d039);  /* 45 */
-  HH(d, a, b, c, M->threetwo[12], S32, 0xe6db99e5); /* 46 */
-  HH(c, d, a, b, M->threetwo[15], S33, 0x1fa27cf8); /* 47 */
-  HH(b, c, d, a, M->threetwo[2], S34, 0xc4ac5665);  /* 48 */
+  HH(a, b, c, d, M->threetwo[5], S31, k[32]);  /* 33 */
+  HH(d, a, b, c, M->threetwo[8], S32, k[33]);  /* 34 */
+  HH(c, d, a, b, M->threetwo[11], S33,k[34]); /* 35 */
+  HH(b, c, d, a, M->threetwo[14], S34,k[35]); /* 36 */
+  HH(a, b, c, d, M->threetwo[1], S31, k[36]);  /* 37 */
+  HH(d, a, b, c, M->threetwo[4], S32, k[37]);  /* 38 */
+  HH(c, d, a, b, M->threetwo[7], S33, k[38]);  /* 39 */
+  HH(b, c, d, a, M->threetwo[10], S34,k[39]); /* 40 */
+  HH(a, b, c, d, M->threetwo[13], S31,k[40]); /* 41 */
+  HH(d, a, b, c, M->threetwo[0], S32, k[41]);  /* 42 */
+  HH(c, d, a, b, M->threetwo[3], S33, k[42]);  /* 43 */
+  HH(b, c, d, a, M->threetwo[6], S34, k[43]);   /* 44 */
+  HH(a, b, c, d, M->threetwo[9], S31, k[44]);  /* 45 */
+  HH(d, a, b, c, M->threetwo[12], S32,k[45]); /* 46 */
+  HH(c, d, a, b, M->threetwo[15], S33,k[46]); /* 47 */
+  HH(b, c, d, a, M->threetwo[2], S34, k[47]);  /* 48 */
 
   /* Round 4 */
-  II(a, b, c, d, M->threetwo[0], S41, 0xf4292244);  /* 49 */
-  II(d, a, b, c, M->threetwo[7], S42, 0x432aff97);  /* 50 */
-  II(c, d, a, b, M->threetwo[14], S43, 0xab9423a7); /* 51 */
-  II(b, c, d, a, M->threetwo[5], S44, 0xfc93a039);  /* 52 */
-  II(a, b, c, d, M->threetwo[12], S41, 0x655b59c3); /* 53 */
-  II(d, a, b, c, M->threetwo[3], S42, 0x8f0ccc92);  /* 54 */
-  II(c, d, a, b, M->threetwo[10], S43, 0xffeff47d); /* 55 */
-  II(b, c, d, a, M->threetwo[1], S44, 0x85845dd1);  /* 56 */
-  II(a, b, c, d, M->threetwo[8], S41, 0x6fa87e4f);  /* 57 */
-  II(d, a, b, c, M->threetwo[15], S42, 0xfe2ce6e0); /* 58 */
-  II(c, d, a, b, M->threetwo[6], S43, 0xa3014314);  /* 59 */
-  II(b, c, d, a, M->threetwo[13], S44, 0x4e0811a1); /* 60 */
-  II(a, b, c, d, M->threetwo[4], S41, 0xf7537e82);  /* 61 */
-  II(d, a, b, c, M->threetwo[11], S42, 0xbd3af235); /* 62 */
-  II(c, d, a, b, M->threetwo[2], S43, 0x2ad7d2bb);  /* 63 */
-  II(b, c, d, a, M->threetwo[9], S44, 0xeb86d391);  /* 64 */
+  II(a, b, c, d, M->threetwo[0], S41, k[48]);  /* 49 */
+  II(d, a, b, c, M->threetwo[7], S42, k[49]);  /* 50 */
+  II(c, d, a, b, M->threetwo[14], S43,k[50]); /* 51 */
+  II(b, c, d, a, M->threetwo[5], S44, k[51]);  /* 52 */
+  II(a, b, c, d, M->threetwo[12], S41,k[52]); /* 53 */
+  II(d, a, b, c, M->threetwo[3], S42, k[53]);  /* 54 */
+  II(c, d, a, b, M->threetwo[10], S43,k[54]); /* 55 */
+  II(b, c, d, a, M->threetwo[1], S44, k[55]);  /* 56 */
+  II(a, b, c, d, M->threetwo[8], S41, k[56]);  /* 57 */
+  II(d, a, b, c, M->threetwo[15], S42,k[57]); /* 58 */
+  II(c, d, a, b, M->threetwo[6], S43, k[58]);  /* 59 */
+  II(b, c, d, a, M->threetwo[13], S44,k[59]); /* 60 */
+  II(a, b, c, d, M->threetwo[4], S41, k[60]);  /* 61 */
+  II(d, a, b, c, M->threetwo[11], S42,k[61]); /* 62 */
+  II(c, d, a, b, M->threetwo[2], S43, k[62]);  /* 63 */
+  II(b, c, d, a, M->threetwo[9], S44, k[63]);  /* 64 */
 
   Hash[0] += a;
   Hash[1] += b;
   Hash[2] += c;
   Hash[3] += d;
-
-
 }
 
 int main(int argc, char *argv[])
@@ -271,7 +268,7 @@ int main(int argc, char *argv[])
 
     //loop through h
     for (int i = 0; i < 4; i++)
-      //print out hash
+          //print out hash
       printf("%02" PRIx32 "", H[i]);
     printf("\n");
     //close file
